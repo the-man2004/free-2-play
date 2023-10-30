6377b9e975d707360559a57f4e13a4e6f83176d9
the-man2004 <heinrichhb111@gmail.com>
2023-10-30 15:35:04 -0700

    Started adding and styling content to About.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,37 @@
 <template>
-  <h2>About</h2>
-  <p>
-    This page was created using Vue.js, JavaScript, pinia (as the store) and
-    tailwindcss (for styling).
-  </p>
+  <div class="min-h-screen bg-slate-900 text-gray-300">
+    <div class="py-12 px-8 max-w-6xl mx-auto md:py-24">
+      <div class="grid grid-cols-1 gap-8 sm:grid-cols-5">
+        <h2
+          class="col-span-2 order-1 font-semibold text-2xl md:order-first md:text-5xl lg:text-6xl"
+        >
+          A quick and easy way to find great games for
+          <span class="text-blue-500 underline">FREE</span>!
+        </h2>
+        <img
+          class="col-span-3 order-first border border-slate-700 md:order-1"
+          src="../assets/img/desktop.png"
+          alt="How the website looks"
+        />
+      </div>
+      <div class="text-center max-w-2xl mx-auto mt-12 md:mt-24">
+        <h3 class="font-semibold text-lg md:text-xl">
+          What is Free-2-play all about?
+        </h3>
+        <p class="mt-8 text-sm md:text-lg">
+          Welcome to Free-2-Play, your ultimate destination for all things free
+          gaming! Dive into a world of endless entertainment without spending a
+          dime as we bring you the latest and greatest free-to-play games across
+          various platforms.
+        </p>
+        <p class="mt-4 text-sm md:text-lg">
+          Discover a treasure trove of gaming options across genres, from
+          action-packed shooters and immersive role-playing adventures to
+          strategic simulations and casual delights. Free-2-Play is your
+          one-stop hub for exploring and enjoying an extensive collection of
+          games that won't cost you a penny to play.
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
