60bcbcf16159c27fde9d78469944f96292d672aa
the-man2004 <heinrichhb111@gmail.com>
2023-10-30 16:56:01 -0700

    Added animations.

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,29 +2,34 @@
   <div class="min-h-screen bg-slate-900 text-gray-300">
     <div class="py-12 px-8 max-w-6xl mx-auto md:py-24">
       <div class="grid grid-cols-1 gap-8 sm:grid-cols-5">
+        <!-- Slide right element -->
         <h1
+          id="slide-right"
           class="col-span-2 order-1 font-semibold text-2xl md:order-first md:text-5xl lg:text-6xl"
         >
           A quick and easy way to find great games for
           <span class="text-blue-500 underline">FREE</span>!
         </h1>
+        <!-- Slide left element -->
         <img
+          id="slide-left"
           class="col-span-3 order-first border border-slate-700 md:order-1"
           src="../assets/img/desktop.png"
           alt="How the website looks"
         />
       </div>
       <div class="text-center max-w-2xl mx-auto mt-12 md:mt-24">
-        <h2 class="font-semibold text-lg md:text-xl">
+        <!-- Slide up elements -->
+        <h2 id="slide-up-1" class="font-semibold text-lg md:text-xl">
           What is Free-2-play all about?
         </h2>
-        <p class="mt-8 text-sm md:text-lg">
+        <p id="slide-up-2" class="opacity-0 mt-8 text-sm md:text-lg">
           Welcome to Free-2-Play, your ultimate destination for all things free
           gaming! Dive into a world of endless entertainment without spending a
           dime as we bring you the latest and greatest free-to-play games across
           various platforms.
         </p>
-        <p class="mt-4 text-sm md:text-lg">
+        <p id="slide-up-3" class="opacity-0 mt-4 text-sm md:text-lg">
           Discover a treasure trove of gaming options across genres, from
           action-packed shooters and immersive role-playing adventures to
           strategic simulations and casual delights. Free-2-Play is your
@@ -80,3 +85,68 @@
     </div>
   </div>
 </template>
+
+<style scopde>
+/* Slide right classes */
+#slide-right {
+  animation-iteration-count: 1;
+  animation: slide-right 0.8s normal forwards ease;
+}
+
+@keyframes slide-right {
+  from {
+    opacity: 0;
+    transform: translateX(-100px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+
+/* Slide left classes */
+#slide-left {
+  animation-iteration-count: 1;
+  animation: slide-left 0.8s normal forwards ease;
+}
+
+@keyframes slide-left {
+  from {
+    opacity: 0;
+    transform: translateX(100px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+
+/* Slide up classes */
+#slide-up-1 {
+  animation-iteration-count: 1;
+  animation: slide-up 0.8s normal forwards ease;
+}
+
+#slide-up-2 {
+  animation-iteration-count: 1;
+  animation: slide-up 0.8s normal forwards ease;
+  animation-delay: 0.2s;
+}
+
+#slide-up-3 {
+  animation-iteration-count: 1;
+  animation: slide-up 0.8s normal forwards ease;
+  animation-delay: 0.4s;
+}
+
+@keyframes slide-up {
+  from {
+    opacity: 0;
+    transform: translateY(100px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+</style>
