73a701cdbcffd67ce70df410c1a07967fb8caf3e
the-man2004 <heinrichhb111@gmail.com>
2023-11-02 14:44:16 -0700

    Added animation to SearchBar.

--- src/components/Search/SearchBar.vue
+++ src/components/Search/SearchBar.vue
@@ -6,56 +6,60 @@
   </div>
   <teleport to="body">
     <!-- Search Modal -->
-    <div
-      v-if="isSearchOpen"
-      @keydown.esc="closeSearch"
-      class="z-50 fixed inset-8 max-w-3xl max-h-[800px] mx-auto flex flex-col rounded-2xl text-gray-300 bg-gray-800"
-    >
-      <div class="flex flex-row p-4">
-        <form @submit.prevent="handleSearch" class="flex flex-row w-full">
-          <i class="fa-solid fa-magnifying-glass text-xl mr-4"></i>
-          <input
-            v-model="searchValue"
-            class="outline-none w-full mr-4 bg-inherit placeholder-gray-300"
-            type="text"
-            placeholder="Search for a game..."
-          />
-        </form>
-        <button
-          @click="closeSearch"
-          class="ml-auto my-auto py-1 px-2 text-center rounded-lg text-xs bg-gray-600"
-        >
-          ESC
-        </button>
-      </div>
+    <transition name="grow">
+      <div
+        v-if="isSearchOpen"
+        @keydown.esc="closeSearch"
+        class="z-50 fixed inset-8 max-w-3xl max-h-[800px] mx-auto flex flex-col rounded-2xl text-gray-300 bg-gray-800"
+      >
+        <div class="flex flex-row p-4">
+          <form @submit.prevent="handleSearch" class="flex flex-row w-full">
+            <i class="fa-solid fa-magnifying-glass text-xl mr-4"></i>
+            <input
+              v-model="searchValue"
+              class="outline-none w-full mr-4 bg-inherit placeholder-gray-300"
+              type="text"
+              placeholder="Search for a game..."
+            />
+          </form>
+          <button
+            @click="closeSearch"
+            class="ml-auto my-auto py-1 px-2 text-center rounded-lg text-xs bg-gray-600"
+          >
+            ESC
+          </button>
+        </div>
 
-      <div class="h-px w-full bg-blue-500"></div>
+        <div class="h-px w-full bg-blue-500"></div>
 
-      <div class="px-4 mt-6 mb-6">
-        <h3 class="text-lg font-semibold">Recommended</h3>
-      </div>
+        <div class="px-4 mt-6 mb-6">
+          <h3 class="text-lg font-semibold">Recommended</h3>
+        </div>
 
-      <div class="px-4 mb-8 overflow-y-auto">
-        <div>
+        <div class="px-4 mb-8 overflow-y-auto">
           <div>
-            <ul>
-              <search-item
-                v-for="(game, i) in gamesStore.defaultSearchResults"
-                :key="i"
-                :game="game"
-                @clicked="closeSearch"
-              ></search-item>
-            </ul>
+            <div>
+              <ul>
+                <search-item
+                  v-for="(game, i) in gamesStore.defaultSearchResults"
+                  :key="i"
+                  :game="game"
+                  @clicked="closeSearch"
+                ></search-item>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </transition>
     <!-- Backdrop -->
-    <div
-      @click="closeSearch"
-      v-if="isSearchOpen"
-      class="z-40 absolute inset-0 backdrop-blur-sm"
-    ></div>
+    <transition name="fade">
+      <div
+        @click="closeSearch"
+        v-if="isSearchOpen"
+        class="z-40 absolute inset-0 backdrop-blur-sm"
+      ></div>
+    </transition>
   </teleport>
 </template>
 
@@ -97,5 +101,35 @@ const handleSearch = () => {
 /* Handle */
 ::-webkit-scrollbar-thumb {
   background: rgb(59 130 246);
+}
+
+/* //////////////////////// */
+/* grow transition styling */
+.grow-enter-from {
+  opacity: 0;
+  transform: scale(0.8);
+}
+.grow-enter-active {
+  transition: all 0.3s ease-in;
+}
+
+.grow-leave-to {
+  opacity: 0;
+  transform: scale(0.8);
+}
+.grow-leave-active {
+  transition: all 0.3s ease-out;
+}
+
+/* /////////////// */
+/* Fade transition */
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
